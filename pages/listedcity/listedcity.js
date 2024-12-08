2b1db133b10c746012ed94a43bf9004c47dc3285
231250004-Yifan Niu <yifanniu@smail.nju.edu.cn>
2024-12-08 09:45:41 +0800

    重写cityPage页面，添加搜索事件；修改所有页面背景

--- pages/listedcity/listedcity.js
+++ pages/listedcity/listedcity.js
@@ -1,58 +1,39 @@
-// pages/listedcity/listedcity.js
 Page({
+  data: {
+    cities: [
+      { name: '北京', weather: '10' },
+      { name: '上海', weather: '20' },
+      { name: '广州', weather: '30' }
+    ],
+    searchQuery: '' // 用户输入城市
+  },
 
-    /**
-     * 组件的属性列表
-     */
-    properties: {
+  // 监听搜索框输入
+  onSearchInput: function (event) {
+    this.setData({
+      searchQuery: event.detail.value
+    });
+  },
 
-    },
+  // 处理点击搜索按钮
+  onSearchClick: function () {
+    //TODO: 
+    //根据用户输入城市，调用api，获取城市温度，添加到cities列表
+    const query = this.data.searchQuery; //用户输入城市
 
-    /**
-     * 组件的初始数据
-     */
-    data: {
-        weatherData:[
-            {city:'南京市鼓楼区',temperature:16,backgroundColor:''},
-            {city:'南京市鼓楼区',temperature:15,backgroundColor:''},
-            {city:'南京市鼓楼区',temperature:14,backgroundColor:''},
-            {city:'南京市鼓楼区',temperature:13,backgroundColor:''},
-            {city:'南京市鼓楼区',temperature:12,backgroundColor:''},
-        ]
-    },
+  },
 
-    /**
-     * 组件的方法列表
-     */
-    methods: {
+  // 处理按下回车键（换行）
+  onSearchConfirm: function () {
+    this.onSearchClick();
+  },
 
-    },
-    onLoad: function() {
-        this.setRandomBackgroundColors();
-      },
-    setRandomBackgroundColors: function() {
-        const updatedWeatherData = this.data.weatherData.map(item => {
-          return {
-            ...item,  // 保留原有的数据
-            backgroundColor: this.randomColor()  // 为每个item生成一个随机背景颜色
-          };
-        });
-        this.setData({
-          weatherData: updatedWeatherData  // 更新weatherData数据
-        });
-    },
-    
-    randomColor: function() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-        const r = parseInt(color.slice(1, 3), 16);
-        const g = parseInt(color.slice(3, 5), 16);
-        const b = parseInt(color.slice(5, 7), 16);
-
-        // 设定透明度为 0.5
-        return `rgba(${r}, ${g}, ${b}, 0.5)`;
-    }
-})
+  // 删除城市
+  onDeleteCity: function (event) {
+    const cityName = event.currentTarget.dataset.city;
+    const newCities = this.data.cities.filter(item => item.name !== cityName);
+    this.setData({
+      cities: newCities
+    });
+  }
+});
